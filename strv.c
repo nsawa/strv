8efb68cbc6cbfa623d2268658d93ab247b3b7a8a
Naoyuki Sawa <sawa_naoyuki-1@yahoo.co.jp>
2016-05-23 00:57:27 +0900

    Prepare empty asm blocks.
    空のアセンブラブロックを準備しました。
    これからアセンブラコードを書いて行きます。

--- strv.c
+++ strv.c
@@ -20,6 +20,7 @@
 //│char** v = strv_new("123","456","789",NULL);
 //│strv_print(v);//123 456 789
 //│strv_free(v);
+#ifndef PIECE
 char** strv_new(const char* s, ...) {
 	char** v;
 	va_list ap;
@@ -28,8 +29,18 @@ char** strv_new(const char* s, ...) {
 	va_end(ap);
 	return v;
 }
+#else //PIECE
+char** strv_new(const char* s, ...);
+asm("
+		.code
+		.align		1
+		.global		strv_new
+strv_new:
+");
+#endif//PIECE
 //-----------------------------------------------------------------------------
 //可変引数の各文字列を複製して、strvを作成する。
+#ifndef PIECE
 char** strv_new_ap(const char* s, va_list ap) {
 	char** v = calloc(1, sizeof(char*));
 	if(!v) { DIE(); }	//メモリ不足
@@ -39,6 +50,15 @@ char** strv_new_ap(const char* s, va_list ap) {
 	}
 	return v;
 }
+#else //PIECE
+char** strv_new_ap(const char* s, va_list ap);
+asm("
+		.code
+		.align		1
+		.global		strv_new_ap
+strv_new_ap:
+");
+#endif//PIECE
 //-----------------------------------------------------------------------------
 //(*pv)の末尾にポインタsを追加する。
 //文字列sは複製されない事に注意せよ。
@@ -49,6 +69,7 @@ char** strv_new_ap(const char* s, va_list ap) {
 //│strv_push(&v,s);
 //│strv_print(v);//123 456 789 ABC
 //│strv_free(v);
+#ifndef PIECE
 int strv_push(char*** pv, char* s) {
 	char** v = *pv;
 	int n = strv_length(v);
@@ -59,6 +80,15 @@ int strv_push(char*** pv, char* s) {
 	*pv = v;
 	return 0;	//当実装は必ず成功(0)を返す。
 }
+#else //PIECE
+int strv_push(char*** pv, char* s);
+asm("
+		.code
+		.align		1
+		.global		strv_push
+strv_push:
+");
+#endif//PIECE
 //-----------------------------------------------------------------------------
 //(*pv)の末尾に文字列sを複製して追加する。
 //オリジナル版の実装はメモリ不足が生じた時に0以外を返すが、当実装は必ず成功(0)を返す。
@@ -67,11 +97,21 @@ int strv_push(char*** pv, char* s) {
 //│strv_extend(&v,"ABC");
 //│strv_print(v);//123 456 789 ABC
 //│strv_free(v);
+#ifndef PIECE
 int strv_extend(char*** pv, const char* s) {
 	char* t = strdup(s);
 	if(!t) { DIE(); }
 	return strv_push(pv, t);
 }
+#else //PIECE
+int strv_extend(char*** pv, const char* s);
+asm("
+		.code
+		.align		1
+		.global		strv_extend
+strv_extend:
+");
+#endif//PIECE
 //-----------------------------------------------------------------------------
 //vを複製し、末尾に文字列sを複製して追加する。
 //vは変更されない。
@@ -82,11 +122,21 @@ int strv_extend(char*** pv, const char* s) {
 //│strv_print(v2);//123 456 789 ABC
 //│strv_free(v1);
 //│strv_free(v2);
+#ifndef PIECE
 char** strv_append(char** v, const char* s) {
 	v = strv_copy(v);
 	strv_extend(&v, s);
 	return v;
 }
+#else //PIECE
+char** strv_append(char** v, const char* s);
+asm("
+		.code
+		.align		1
+		.global		strv_append
+strv_append:
+");
+#endif//PIECE
 //-----------------------------------------------------------------------------
 //v1とv2の深いコピーを連結した、新しいstrvを作成する。
 //v1もv2も変更されない。
@@ -100,6 +150,7 @@ char** strv_append(char** v, const char* s) {
 //│strv_free(v1);
 //│strv_free(v2);
 //│strv_free(v3);
+#ifndef PIECE
 char** strv_merge(char** v1, char** v2) {
 	char* s;
 	v1 = strv_copy(v1);		//v1の深いコピーを作成する。
@@ -108,6 +159,15 @@ char** strv_merge(char** v1, char** v2) {
 	}
 	return v1;
 }
+#else //PIECE
+char** strv_merge(char** v1, char** v2);
+asm("
+		.code
+		.align		1
+		.global		strv_merge
+strv_merge:
+");
+#endif//PIECE
 //-----------------------------------------------------------------------------
 //vの中の、文字列sと同じ内容の文字列を全て開放する。
 //vを変更するが、縮小方向の変更であり、メモリの再割り当ては行わない。
@@ -118,6 +178,7 @@ char** strv_merge(char** v1, char** v2) {
 //│strv_remove(v,"456");
 //│strv_print(v);//123 789
 //│strv_free(v);
+#ifndef PIECE
 char** strv_remove(char** v, const char* s) {
 	char** i = v;
 	char** j = v;
@@ -132,6 +193,15 @@ char** strv_remove(char** v, const char* s) {
 	*j = NULL;
 	return v;	//引数vをそのまま返す。
 }
+#else //PIECE
+char** strv_remove(char** v, const char* s);
+asm("
+		.code
+		.align		1
+		.global		strv_remove
+strv_remove:
+");
+#endif//PIECE
 //-----------------------------------------------------------------------------
 //vの中の、重複する文字列を全て開放する。
 //vを変更するが、縮小方向の変更であり、メモリの再割り当ては行わない。
@@ -141,6 +211,7 @@ char** strv_remove(char** v, const char* s) {
 //│strv_uniq(v);
 //│strv_print(v);//123 456 789
 //│strv_free(v);
+#ifndef PIECE
 char** strv_uniq(char** v) {
 	char** i = v;
 	char* t;
@@ -149,6 +220,15 @@ char** strv_uniq(char** v) {
 	}
 	return v;	//引数vをそのまま返す。
 }
+#else //PIECE
+char** strv_uniq(char** v);
+asm("
+		.code
+		.align		1
+		.global		strv_uniq
+strv_uniq:
+");
+#endif//PIECE
 //-----------------------------------------------------------------------------
 //vの中から、文字列sと同じ内容の文字列を検索する。
 //見つかったら、その文字列のポインタを返す。
@@ -158,6 +238,7 @@ char** strv_uniq(char** v) {
 //│printf("%s\n",strv_find(v,"456"));//456
 //│printf("%s\n",strv_find(v,"567"));//(null)
 //│strv_free(v);
+#ifndef PIECE
 char* strv_find(char** v, const char* s) {
 	char* t;
 	while((t = *v++)) {
@@ -165,6 +246,15 @@ char* strv_find(char** v, const char* s) {
 	}
 	return t;
 }
+#else //PIECE
+char* strv_find(char** v, const char* s);
+asm("
+		.code
+		.align		1
+		.global		strv_find
+strv_find:
+");
+#endif//PIECE
 //-----------------------------------------------------------------------------
 //v1とv2の中に、同じ内容の文字列が有るかを検索する。
 //有れば1、無ければ0を返す。
@@ -177,6 +267,7 @@ char* strv_find(char** v, const char* s) {
 //│strv_free(v1);
 //│strv_free(v2);
 //│strv_free(v3);
+#ifndef PIECE
 int strv_overlap(char** v1, char** v2) {
 	char* s;
 	while((s = *v1++)) {
@@ -184,6 +275,15 @@ int strv_overlap(char** v1, char** v2) {
 	}
 	return 0;
 }
+#else //PIECE
+int strv_overlap(char** v1, char** v2);
+asm("
+		.code
+		.align		1
+		.global		strv_overlap
+strv_overlap:
+");
+#endif//PIECE
 //-----------------------------------------------------------------------------
 //vをソートする。
 //vを変更するが、要素数は変化せず、メモリの再割り当ては行わない。
@@ -193,6 +293,7 @@ int strv_overlap(char** v1, char** v2) {
 //│strv_sort(v);
 //│strv_print(v);//123 456 ABC DEF
 //│strv_free(v);
+#ifndef PIECE
 static int strv_sort_compar(const void* _x, const void* _y) {
 	char* x = *(char**)_x;
 	char* y = *(char**)_y;
@@ -202,11 +303,30 @@ char** strv_sort(char** v) {
 	qsort(v, strv_length(v), sizeof(char*), strv_sort_compar);
 	return v;
 }
+#else //PIECE
+char** strv_sort(char** v);
+asm("
+		.code
+		.align		1
+		.global		strv_sort
+strv_sort:
+");
+#endif//PIECE
 //-----------------------------------------------------------------------------
 //vの各文字列を、標準出力に一行ずつ表示する。
+#ifndef PIECE
 void strv_print(char** v) {
 	char* s;
 	while((s = *v++)) {
 		puts(s);
 	}
 }
+#else //PIECE
+void strv_print(char** v);
+asm("
+		.code
+		.align		1
+		.global		strv_print
+strv_print:
+");
+#endif//PIECE
